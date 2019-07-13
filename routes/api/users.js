cb1b659325fd669c7d89cc3041d2e12131be0d7e
Kavya Kumar <tellkavya@gmail.com>
2019-07-13 16:40:56 -0700

    Add jwt

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,10 +2,13 @@ const express = require("express");
 const router = express.Router();
 const User = require('../../models/User');
 const bcrypt = require("bcryptjs");
+const keys = require("../../config/keys");
+const jwt = require('jsonwebtoken');
 
 router.get("/test", (req,res) => {
     res.json({ msg: "This is the user route"});
 });
+
 
 router.post('/register', (req ,res) => {
     User.findOne( { username: req.body.username })
@@ -46,7 +49,23 @@ router.post('/login', (req, res) => {
             bcrypt.compare(password, user.password)
                 .then(isMatch => {
                     if(isMatch) {
-                        res.json({ msg: "Success!" });
+                        const payload = {
+                            id: user.id,
+                            username: user.username,
+                            age: user.age
+                        }
+                        jwt.sign(
+                            payload,
+                            keys.secretOrKey,
+                            { expiresIn: 3600 },
+                            (err, token) => {
+                                res.json({
+                                    success: true,
+                                    token: "Bearer " + token
+                                });
+                            }
+                        );
+
                     } else {
                         return res.status(400).json({ password: "Incorrect password"});
                     }
