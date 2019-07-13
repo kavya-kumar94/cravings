2b55ea3da98cf125b121647ee1396b2cb5b938c1
Kavya Kumar <tellkavya@gmail.com>
2019-07-13 16:21:51 -0700

    Add models, routes, and passport

--- app.js
+++ app.js
@@ -4,13 +4,28 @@ const mongoose = require("mongoose");
 const db = require("./config/keys").mongoURI
 const users = require("./routes/api/users");
 const restaurants = require("./routes/api/restaurants");
+const User = require("./models/User");
+const bodyParser = require('body-parser');
 
 mongoose
     .connect(db, { useNewUrlParser: true })
     .then(() => console.log("Connected to mongoDB"))
     .catch(err => console.log(err));
 
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
+    const user = new User({
+        handle: "jim",
+        email: "jim@jim.jim",
+        password: "jimisgreat123"
+    })
+    user.save()
     res.send("Hello Sophia");
 });
 
@@ -19,4 +34,4 @@ app.use("/api/restaurants", restaurants)
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {console.log(`Listening on port ${port}`)});
+app.listen(port, () => console.log(`Listening on port ${port}`));
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,57 @@
 const express = require("express");
 const router = express.Router();
+const User = require('../../models/User');
+const bcrypt = require("bcryptjs");
 
 router.get("/test", (req,res) => {
     res.json({ msg: "This is the user route"});
+});
+
+router.post('/register', (req ,res) => {
+    User.findOne( { email: req.body.email })
+    .then(user => {
+        if (user) {
+            return res.startus(400).json({ email: "A user is already registered with that email"})
+        } else {
+            const newUser = new User({
+                handle: req.body.handle,
+                email: req.body.email,
+                password: req.body.password
+            })
+
+            // newUser.save().then(user => res.send(user)).catch(err => res.send(err));
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if(err) throw err;
+                    newUser.password = hash;
+                    newUser.save()
+                        .then((user) => res.json(user))
+                        .catch(err => console.log(err))
+                })
+            })
+        }
+    })
+})
+
+router.post('/login', (req, res) => {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    User.findOne({ email })
+        .then(user => {
+            if(!user) {
+                return res.status(404).json({ email: "This user does not exist." });
+            }
+
+            bcrypt.compare(password, user.password)
+                .then(isMatch => {
+                    if(isMatch) {
+                        res.json({ msg: "Success!" });
+                    } else {
+                        return res.status(400).json({ password: "Incorrect password"});
+                    }
+                })
+        })
 })
 
 module.exports = router;
