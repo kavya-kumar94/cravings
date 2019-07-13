a61f44bfd307be77793f617545f489e7a9071cef
mkim <x1michelleskim@gmail.com>
2019-07-13 13:02:55 -0700

    Add users and restaurants routes

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI
+const users = require("./routes/api/users");
+const restaurants = require("./routes/api/restaurants");
+
+mongoose
+    .connect(db, { useNewUrlParser: true })
+    .then(() => console.log("Connected to mongoDB"))
+    .catch(err => console.log(err));
 
 app.get("/", (req, res) => {
-    res.send("Hello Kavya");
+    res.send("Hello Sophia");
 });
+
+app.use("/api/users", users)
+app.use("/api/restaurants", restaurants)
 
 const port = process.env.PORT || 5000;
 
