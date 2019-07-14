729aba5418bda712a27c0a841082611c5a94cbbe
Selam Weldu <ssweldu@gmail.com>
2019-07-13 21:20:52 -0700

    add nav bar component

--- frontend/src/components/main/main_page.js
+++ frontend/src/components/main/main_page.js
@@ -5,10 +5,10 @@ class MainPage extends React.Component {
     render() {
         return (
             <div>
-                <h1>A Twitter Clone</h1>
+                <h1>Cravings</h1>
                 <footer>
-                    Copyright &copy; 2019 Chirper
-        </footer>
+                    We are live!! React not broken, Everything is fine
+                </footer>
             </div>
         );
     }
--- frontend/src/components/nav/navbar.js
+++ frontend/src/components/nav/navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
-import './navbar.css'
+import { Link } from 'react-router-dom';
+// import './navbar.css'
 
 class NavBar extends React.Component {
     constructor(props) {
@@ -14,14 +14,10 @@ class NavBar extends React.Component {
         this.props.logout();
     }
 
-    // Selectively render links dependent on whether the user is logged in
     getLinks() {
         if (this.props.loggedIn) {
             return (
                 <div>
-                    <Link to={'/tweets'}>All Tweets</Link>
-                    <Link to={'/profile'}>Profile</Link>
-                    <Link to={'/new_tweet'}>Write a Tweet</Link>
                     <button onClick={this.logoutUser}>Logout</button>
                 </div>
             );
@@ -38,7 +34,7 @@ class NavBar extends React.Component {
     render() {
         return (
             <div>
-                <h1>Chirper</h1>
+                <h1>Cravings NavBar</h1>
                 {this.getLinks()}
             </div>
         );
