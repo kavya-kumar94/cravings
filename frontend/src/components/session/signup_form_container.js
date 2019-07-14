a94c165124186be9eaa95ab0d32d3df516e62b04
mkim <x1michelleskim@gmail.com>
2019-07-14 14:10:06 -0700

    Add modal actions to signup form container

--- frontend/src/components/session/signup_form_container.js
+++ frontend/src/components/session/signup_form_container.js
@@ -1,6 +1,8 @@
+import React from 'react';
 import { connect } from 'react-redux';
 import { signup } from '../../actions/session_actions';
 import SignupForm from './signup_form';
+import { openModal, closeModal } from '../../actions/modal_actions';
 
 const mapStateToProps = (state) => {
     return {
@@ -11,11 +13,14 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        signup: user => dispatch(signup(user))
+        signup: user => dispatch(signup(user)),
+        otherForm: (
+            <button onClick={() => dispatch(openModal('login'))}>
+                Log in
+            </button>
+        ),
+        closeModal: () => dispatch(closeModal())
     }
 }
 
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps
-)(SignupForm);
+export default connect(mapStateToProps, mapDispatchToProps)(SignupForm);
