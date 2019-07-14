24a82aad83a57ee3efcc636593b0e2c26ae9b2f0
mkim <x1michelleskim@gmail.com>
2019-07-14 14:15:54 -0700

    Import modal component to app file and render component

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { AuthRoute } from '../util/route_util';
 import { Switch } from 'react-router-dom';
 import NavBarContainer from './nav/navbar_container';
+import Modal from '../components/modal/modal';
 
 import MainPage from './main/main_page';
 import FoodsContainer from './foods/foods_container';
@@ -9,6 +10,7 @@ import FoodsContainer from './foods/foods_container';
 const App = () => (
     <div>
         <NavBarContainer />
+        <Modal />
         <Switch>
             <AuthRoute exact path="/" component={MainPage} />
 
