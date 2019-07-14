754fd1d6961430fc39e154f918b7d6e7d24ae205
mkim <x1michelleskim@gmail.com>
2019-07-14 14:14:58 -0700

    Remove routes for signup and login forms

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -4,8 +4,6 @@ import { Switch } from 'react-router-dom';
 import NavBarContainer from './nav/navbar_container';
 
 import MainPage from './main/main_page';
-import LoginFormContainer from './session/login_form_container';
-import SignupFormContainer from './session/signup_form_container';
 import FoodsContainer from './foods/foods_container';
 
 const App = () => (
@@ -13,8 +11,6 @@ const App = () => (
         <NavBarContainer />
         <Switch>
             <AuthRoute exact path="/" component={MainPage} />
-            <AuthRoute exact path="/login" component={LoginFormContainer} />
-            <AuthRoute exact path="/signup" component={SignupFormContainer} />
 
             <ProtectedRoute exact path="/foods" component={FoodsContainer} />
         </Switch>
