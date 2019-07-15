0f2c5ad34cef79d517a21d8737513775c655f843
mkim <x1michelleskim@gmail.com>
2019-07-14 18:31:30 -0700

    Add drinks to seeder file and update Drink Schema/model

--- models/Drink.js
+++ models/Drink.js
@@ -3,8 +3,24 @@ const Schema = mongoose.Schema;
 
 
 const DrinkSchema = new Schema({
+    id: {
+        type: String,
+        required: true
+    },
     name: {
         type: String,
+        required: true
+    },
+    imageUrl: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    rating: {
+        type: Number,
         required: true
     },
     lat: {
@@ -15,70 +31,91 @@ const DrinkSchema = new Schema({
         type: Number,
         required: true
     },
+    price: {
+        type: String,
+        // required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
     city: {
         type: String,
         required: true
     },
-    caffeine: {
-        type: Boolean,
+    zipCode: {
+        type: String,
         required: true
     },
-    sweet: {
-        type: Boolean,
+    country: {
+        type: String,
         required: true
     },
-    aromatic: {
-        type: Boolean,
+    state: {
+        type: String,
         required: true
     },
-    sparkling: {
-        type: Boolean,
-        required: true
+    phone: {
+        type: String,
+        // required: true
     },
-    hot: {
-        type: Boolean,
-        required: true
-    },
-    iced: {
-        type: Boolean,
-        required: true
-    },
-    healthy: {
-        type: Boolean,
-        required: true
-    },
-    sad: {
-        type: Boolean,
-        required: true
-    },
-    happy: {
-        type: Boolean,
-        required: true
-    },
-    angry: {
-        type: Boolean,
-        required: true
-    },
-    sick: {
-        type: Boolean,
-        required: true
-    },
-    celebratory: {
-        type: Boolean,
-        required: true
-    },
-    stressed: {
-        type: Boolean,
-        required: true
-    },
-    adventurous: {
-        type: Boolean,
-        required: true
-    },
-    price: {
-        type: Number,
-        required: true
-    }
+    // ATTRIBUTES: FEELINGS/CRAVINGS:
+    // caffeine: {
+    //     type: Boolean,
+    //     required: true
+    // },
+    // sweet: {
+    //     type: Boolean,
+    //     required: true
+    // },
+    // aromatic: {
+    //     type: Boolean,
+    //     required: true
+    // },
+    // hot: {
+    //     type: Boolean,
+    //     required: true
+    // },
+    // iced: {
+    //     type: Boolean,
+    //     required: true
+    // },
+    // healthy: {
+    //     type: Boolean,
+    //     required: true
+    // },
+    // sad: {
+    //     type: Boolean,
+    //     required: true
+    // },
+    // tired: {
+    //     type: Boolean,
+    //     required: true
+    // },
+    // happy: {
+    //     type: Boolean,
+    //     required: true
+    // },
+    // angry: {
+    //     type: Boolean,
+    //     required: true
+    // },
+    // sick: {
+    //     type: Boolean,
+    //     required: true
+    // },
+    // celebratory: {
+    //     type: Boolean,
+    //     required: true
+    // },
+    // stressed: {
+    //     type: Boolean,
+    //     required: true
+    // },
+    // adventurous: {
+    //     type: Boolean,
+    //     required: true
+    // }
 })
 
 const Drink = mongoose.model('drinks', DrinkSchema);
