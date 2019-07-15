adea8aee105a3ed494912690e3a22f5a28d3870e
mkim <x1michelleskim@gmail.com>
2019-07-15 16:42:23 -0700

    testing axios get request and correct seed file for Yelp API

--- frontend/src/util/drink_api_util.js
+++ frontend/src/util/drink_api_util.js
@@ -7,3 +7,22 @@ export const fetchDrinks = () => {
 export const fetchDrink = (id) => {
     return axios.get(`/api/drinks/${id}`)
 };
+
+
+// export const fetchDrinks = (userData) => {
+//     return axios.get('/api/drinks',
+//     query: {
+            // tired: userData.tired,
+            // sweet: userData.sweet,
+
+
+//      })
+// }
+
+// export const fetchDrinks = () => {
+//     return axios.get('api/drinks',
+//         params: {
+//             id: 123
+//         }
+//     )
+// }
