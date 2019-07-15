240ce0bdf1fa965315a8408cf120b39358a79486
Kavya Kumar <tellkavya@gmail.com>
2019-07-15 11:40:26 -0700

    Add route

--- frontend/src/components/food_drink.jsx
+++ frontend/src/components/food_drink.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 import './food_drink.css'
-class FoodDrink extends React.Component {
 
-    foodWheel() {
-        // e.preventDefault();
-        // this.props.history.push('/foodwheel')
+class FoodDrink extends React.Component {
+    constructor(props) {
+        super(props);
+        this.foodWheel = this.foodWheel.bind(this);
+    }
+
+    foodWheel(e) {
+        e.preventDefault();
+        this.props.history.push('/foodwheel')
     }
 
     render() {
         return (
             <div className="food-drink">
                 <h3>Would you like food, or drinks?</h3>
-                <button onClick={this.foodWheel()}className="food-btn">Food</button>
+                <button onClick={this.foodWheel}className="food-btn">Food</button>
                 <button className="drink-btn">Drinks</button>
             </div>
         )
     }
 }
 
-export default FoodDrink;
+export default withRouter(FoodDrink);
