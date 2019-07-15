515d5a6fb1730487d49f189002b1ee6c0c79f88d
Sophia Cheung <cheungsinhan@gmail.com>
2019-07-14 21:24:11 -0700

    Fix user auth signup component

--- frontend/src/util/session_api_util.js
+++ frontend/src/util/session_api_util.js
@@ -9,9 +9,9 @@ export const setAuthToken = token => {
 };
 
 export const signup = (userData) => {
-    return axios.post('api/users/register', userData);
+    return axios.post('/api/users/register', userData);
 };
 
 export const login = (userData) => {
-    return axios.post('api/users/login', userData);
+    return axios.post('/api/users/login', userData);
 };
