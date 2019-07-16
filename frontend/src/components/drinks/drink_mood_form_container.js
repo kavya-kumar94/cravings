40297ff11ddcc20226728a7df4a978574bc8dd07
mkim <x1michelleskim@gmail.com>
2019-07-16 09:27:02 -0700

    Update food and drink form containers to pass in query as argument for fetchFoods and fetchDrinks in mapDispatchToProps

--- frontend/src/components/drinks/drink_mood_form_container.js
+++ frontend/src/components/drinks/drink_mood_form_container.js
@@ -14,4 +14,4 @@ const mapDispatchToProps = dispatch => {
     }
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(DrinkForm)
+export default connect(mapStateToProps, mapDispatchToPfe 
