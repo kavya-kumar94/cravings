946302b0a2b75937a21632df65fa29a50422f636
Kavya Kumar <tellkavya@gmail.com>
2019-07-16 09:35:10 -0700

    Query string for drink wheel

--- frontend/src/components/drinks/drink_mood_wheel.jsx
+++ frontend/src/components/drinks/drink_mood_wheel.jsx
@@ -55,7 +55,7 @@ class DrinkWheel extends React.Component {
         this.props.history.push({
             pathname: '/drinks',
             // search: `?caffeine=${this.state.caffeine}&sweet=${this.state.sweet}&aromatic=${this.state.aromatic}&hot=${this.state.hot}&iced=${this.state.iced}&healthy=${this.state.healthy}&sad=${this.state.sad}&tired=${this.state.tired}&happy=${this.state.happy}&angry=${this.state.angry}&sick=${this.state.sick}&celebratory=${this.state.celebratory}&stressed=${this.state.stressed}&adventurous=${this.state.adventurous}`
-            search: `?caffeine=${this.state.caffeine}`
+            search: `?caffeine=${this.state.caffeine}&sweet=${this.state.sweet}`
             })
     }
 
