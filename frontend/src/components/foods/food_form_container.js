ca05aa2c39e7c76ec8e6d92cc4f10ed412b879ef
mkim <x1michelleskim@gmail.com>
2019-07-15 22:36:25 -0700

    Update FoodForm container for mapDispatchToProps

--- frontend/src/components/foods/food_form_container.js
+++ frontend/src/components/foods/food_form_container.js
@@ -10,7 +10,7 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = dispatch => {
     return {
-        fetchFoods: () => dispatch(fetchFoods())
+        fetchFoods: (parsed) => dispatch(fetchFoods(parsed))
     };
 };
 
