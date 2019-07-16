08230f111f9861b84289493eda651c2fe5a44914
mkim <x1michelleskim@gmail.com>
2019-07-16 16:29:56 -0700

    Update drink index to include a randomizer button and redirect button to form if no results

--- frontend/src/components/drinks/drinks_index.jsx
+++ frontend/src/components/drinks/drinks_index.jsx
@@ -6,32 +6,57 @@ import './drinks.css';
 
 class Drinks extends React.Component {
 
+    constructor(props) {
+        super(props);
+        
+        this.randomizer = this.randomizer.bind(this);
+    }
+
     componentWillMount() {
         this.props.fetchDrinks(this.props.location.search);
     }
 
+    randomizer() {
+        let num_choices = this.props.drinks.length;
+        let choiceIdx = Math.floor(Math.random() * Math.floor(num_choices));
+        let drinkId = this.props.drinks[choiceIdx].id;
+        this.props.history.push(`/drinks/${drinkId}`);
+    }
+
     render() {
 
-        console.log('this is the location.search in drinks index')
-        console.log(this.props.location.search);
+        if (this.props.drinks === undefined) {
+            return null;
+        } else if (Object.keys(this.props.drinks).length === 0) {
+            return (
+                <div className='drinks-index-container'>
+                    <div className='no-results-for-drink'>
+                        <h3>We were unable to find drinks in your area that meet your cravings.</h3>
+                        <button className='spin-again' onClick={() => this.props.history.push('/drinkwheel')}>Please try again.</button>
+                    </div>
+                </div>
+            )
+        } else {
+            const drinks = this.props.drinks.map(drink => (
+                <DrinksIndexItem
+                    key={drink.id}
+                    drink={drink} />
+            ));
 
-        if (this.props.drinks === undefined) return null;
+            return (
+                <div className="drinks-index-container">
 
-        const drinks = this.props.drinks.map(drink => (
-            <DrinksIndexItem
-                key={drink.id}
-                drink={drink} />
-        ));
+                    <ul className="drinks-ul">
+                        {drinks}
+                    </ul>
 
-        return (
-            <div className="drinks-index-container">
+                    <button className='pick-one' onClick={() => this.randomizer()}>
+                        Pick for me, I am indecisive
+                    </button>
 
-                <ul className="drinks-ul">
-                    {drinks}
-                </ul>
-
-            </div>
-        );
+                </div>
+            );
+        }
     }
     
 }
