ba3ac095afbdcb79f40bec3b965c93040ccbab24
Sophia Cheung <cheungsinhan@gmail.com>
2019-07-15 23:46:55 -0700

    Finish fixing wheel legend onclick functionality

--- frontend/src/components/drinks/drink_mood_form.jsx
+++ frontend/src/components/drinks/drink_mood_form.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import './drink_mood_form.css'
-import queryString from 'query-string';
+// import queryString from 'query-string';
 
 class DrinkForm extends React.Component {
     constructor(props) {
