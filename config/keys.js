72ab02f9734765b479799bfe583a7b254acf7917
Kavya Kumar <tellkavya@gmail.com>
2019-07-17 11:45:59 -0700

    Add keys to gitignore

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: "mongodb+srv://admin:QuIwYeYOO4s7BExg@merncluster-5j2iu.mongodb.net/test?retryWrites=true&w=majority",
+    secretOrKey: "kdjfaere0we7yr0er9wepr0887te",
+    YelpApiKey: "nYHIXBZJ5v8UFRI5nnG8So8pqJmDrC9ZsOy9rPjDM1ntm6EQ1mTi-bdJhxZs585BqrThjKKpHi0y2Z-6fmAx48-1SWYNUa3vWgr1JkirCLkz-3x-vYUk81fVT7orXXYx"
+}
