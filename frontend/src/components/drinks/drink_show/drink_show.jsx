0a72ed843b7aba228a36d4669e27dbf3bf77e1e4
Kavya Kumar <tellkavya@gmail.com>
2019-07-17 09:07:51 -0700

    Add white background to photos

--- frontend/src/components/drinks/drink_show/drink_show.jsx
+++ frontend/src/components/drinks/drink_show/drink_show.jsx
@@ -18,9 +18,11 @@ class DrinkShow extends React.Component {
                 <p>{drink.name}</p>
                 <p>{drink.address}, {drink.city}, {drink.state} {drink.zipCode}</p>
                 <p>{drink.price}</p>
-                <div className={`rating-static rating-${drink.rating * 10}`}>
+                <div className="photo-rating">
+                    <div className={`rating-static rating-${drink.rating * 10}`}>
+                    </div>
+                    <img src={drink.imageUrl} className="drink-photo" />
                 </div>
-                <img src={drink.imageUrl} className="drink-photo" />
             </div>
         )
     };
