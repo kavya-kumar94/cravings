f8256562e0f4dedcb5a25e0aa0401375c9cc267e
Sophia Cheung <cheungsinhan@gmail.com>
2019-08-05 15:06:48 -0700

    Add app.js

--- config/keys.js
+++ config/keys.js
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
-module.exports = {
-    mongoURI: "mongodb+srv://admin:QuIwYeYOO4s7BExg@merncluster-5j2iu.mongodb.net/test?retryWrites=true&w=majority",
-    secretOrKey: "kdjfaere0we7yr0er9wepr0887te",
-    YelpApiKey: "nYHIXBZJ5v8UFRI5nnG8So8pqJmDrC9ZsOy9rPjDM1ntm6EQ1mTi-bdJhxZs585BqrThjKKpHi0y2Z-6fmAx48-1SWYNUa3vWgr1JkirCLkz-3x-vYUk81fVT7orXXYx"
-=======
 if (process.env.NODE_ENV === 'production') {
     module.exports = require('./keys_prod');
 } else {
     module.exports = require('./keys_dev');
->>>>>>> b35fd60f5b4c270aa92a5a3b7885a3594004a664
 }
