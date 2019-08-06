410d678f5d84c807c46ac85140377dbcf6b721bc
mkim <x1michelleskim@gmail.com>
2019-08-06 09:31:53 -0700

    Add loading page to food show container

--- frontend/src/components/foods/food_show/food_show.jsx
+++ frontend/src/components/foods/food_show/food_show.jsx
@@ -1,30 +1,44 @@
 import React from 'react';
 
+//loading:
+
+import LoadingIcon from '../../loading/loading_icon';
+import '../../loading/loading.css';
 
 class FoodShow extends React.Component{
 
+
     componentDidMount(){
-        this.props.fetchFood(this.props.match.params.foodId)
+        this.props.fetchFood(this.props.match.params.foodId);
     }
 
     render(){
         
         const { food } = this.props;
-
-        if (this.props.food === undefined) return null;
-
-        return(
-            <div className="food-show">
-                <p>{food.name}</p>
-                <p>{food.address}, {food.city}, {food.state} {food.zipCode}</p>
-                <p>{food.price}</p>
-                <div className="photo-rating">
-                    <div className={`rating-static rating-${food.rating*10}`}>
+        if (food === undefined) return null;
+        if (this.props.loading) {
+            return <LoadingIcon />
+        }
+        // if (this.props.loading) {
+        //     console.log('detail loading');
+        //     return <LoadingIcon />
+        // } else {
+            console.log('i mounted!')
+            return(
+                <div className="food-show">
+                    <p>{food.name}</p>
+                    <p>{food.address}, {food.city}, {food.state} {food.zipCode}</p>
+                    <p>{food.price}</p>
+                    <div className="photo-rating">
+                        <div className={`rating-static rating-${food.rating*10}`}>
+                        </div>
+                        <img src={this.props.food.imageUrl} className="food-photo" />
                     </div>
-                    <img src={this.props.food.imageUrl} className="food-photo" />
                 </div>
-            </div>
-        )
+            )
+
+        // }
+
     }
 }
 
