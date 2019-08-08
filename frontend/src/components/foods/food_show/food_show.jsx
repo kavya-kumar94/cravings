aed3b48fc99c1c84a17fc96ea834bb2c741c5e67
mkim <x1michelleskim@gmail.com>
2019-08-08 16:41:24 -0700

    Finish styling map for food index and show page

--- frontend/src/components/foods/food_show/food_show.jsx
+++ frontend/src/components/foods/food_show/food_show.jsx
@@ -73,8 +73,8 @@ class FoodShow extends React.Component {
               </div>
           </div>
 
-          <div className="drink-map">
-            <Map center={[food.lat, food.lng]} zoom={13} style={{ height: "300px", width: "300px", position: "absolute" }}>
+          <div className="food-map-container">
+            <Map className="food-map" center={[food.lat, food.lng]} zoom={13} style={{ height: "250px", width: "798px", position: "absolute" }}>
               <TileLayer
                 attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
                 url="https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png"
