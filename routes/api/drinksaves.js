f248fcc03273992e30113a8fa65cc0d8aad0338a
Kavya Kumar <tellkavya@gmail.com>
2019-08-08 19:37:37 -0700

    Add get drinksave to drinksaves.js

--- routes/api/drinksaves.js
+++ routes/api/drinksaves.js
@@ -1,16 +1,33 @@
 const express = require("express");
 const router = express.Router();
 const DrinkSave = require('../../models/DrinkSave');
+const mongoose = require("mongoose");
 
 router.get('/',
     (req, res) => {
-        DrinkSave.find({ userId: req.body.userId })
-            .then(saves => {
-                res.json(saves);
+        DrinkSave.find({ userId: mongoose.Types.ObjectId(req.body.userId )})
+            // .then(saves => {
+            //     res.json(saves);
+            // })
+            // .catch(err => res.status(404).json('No saves found'));
+            .populate('drinkId')
+            .exec()
+            .then(drinks => {
+                res.json(drinks);
             })
-            .catch(err => res.status(404).json('No saves found'));
     }
 );
+router.get('/:drinkId',
+    (req, res) => {
+        DrinkSave.find({
+            userId: mongoose.Types.ObjectId(req.query.userId),
+            drinkId: mongoose.Types.ObjectId(req.params.drinkId)
+        }).then(saves => {
+            res.json(saves[0]);
+        })
+            .catch(err => res.status(404).json("No save found"));
+    }
+)
 
 router.post("/",
     (req, res) => {
