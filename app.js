54b99721b381150bc80b945b56ef0db0c39237a4
mkim <x1michelleskim@gmail.com>
2019-08-09 18:58:00 -0700

    Add port for production

--- app.js
+++ app.js
@@ -1,35 +1,37 @@
 // const seed = require('./seeder');
-const path = require('path');
+const path = require("path");
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const db = require("./config/keys").mongoURI
+const db = require("./config/keys").mongoURI;
 const users = require("./routes/api/users");
 const foods = require("./routes/api/foods");
 const drinks = require("./routes/api/drinks");
 const rooms = require("./routes/api/rooms");
-const bodyParser = require('body-parser');
-const foodsaves = require('./routes/api/foodsaves');
-const drinksaves = require('./routes/api/drinksaves');
+const bodyParser = require("body-parser");
+const foodsaves = require("./routes/api/foodsaves");
+const drinksaves = require("./routes/api/drinksaves");
 
 mongoose
-    .connect(db, { useNewUrlParser: true } )
-    .then(() => console.log("Connected to mongoDB"))
-    .catch(err => console.log(err));
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("Connected to mongoDB"))
+  .catch(err => console.log(err));
 
-app.use(bodyParser.urlencoded({
+app.use(
+  bodyParser.urlencoded({
     extended: false
-}));
+  })
+);
 app.use(bodyParser.json());
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('frontend/build'));
-    app.get('/', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-    })
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
 }
 
-var server = app.listen(5000);
+var server = app.listen(process.env.PORT || 5000);
 // var io = require('socket.io').listen(server);
 
 // io.on('connection', socket => {
@@ -38,7 +40,7 @@ var server = app.listen(5000);
 //     socket.on('disconnect', function () {
 //         console.log('user disconnected');
 //     });
-    
+
 //     // socket.on('chat', ({msg}) => {
 //     //     io.emit(msg);
 //     //     console.log(msg);
@@ -49,11 +51,11 @@ var server = app.listen(5000);
 //     })
 // });
 
-// seed(); 
+// seed();
 
-app.use("/api/users", users)
-app.use("/api/foods", foods)
-app.use("/api/drinks", drinks)
-app.use("/api/rooms", rooms)
-app.use("/api/foodsaves", foodsaves)
-app.use("/api/drinksaves", drinksaves)
+app.use("/api/users", users);
+app.use("/api/foods", foods);
+app.use("/api/drinks", drinks);
+app.use("/api/rooms", rooms);
+app.use("/api/foodsaves", foodsaves);
+app.use("/api/drinksaves", drinksaves);
